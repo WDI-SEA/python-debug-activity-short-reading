36493d18ffdcd63fef064f07e1ff98562752993a
kys3923 <kys3923@gmail.com>
2021-03-01 21:04:27 -0500

    figured out problem 1

--- problem1.py
+++ problem1.py
@@ -2,36 +2,30 @@ offense = False
 defense = False
 rule_changes = False
 
-print("How are the Jags doing?\n")
-
 def get_offense():
+  global offense
   offense = True
-  print("We have offense:", offense)
 
 def get_defense():
-  offense = True
+  global defense
   defense = True
-  print("We have defense:", defense)
 
   def get_rule_changes():
+    global rule_changes
     rule_changes = True
-    print("We have some rule changes:", rule_changes)
-
-    if offense and defense and rule_changes:
-      print("We're going to the Super Bowl!")
-    else:
-      print("I can't predict the future, but no, the Jaguars will never win the Super Bowl.")
 
   if offense and defense:
     get_rule_changes()
-  
-  
 
 get_offense()
 get_defense()
 
+print("How are the Jags doing?\n")
+print("We have offense:", offense)
+print("We have defense:", defense)
+print("We have some rule changes:", rule_changes)
 
-
-
-
-
+if offense and defense and rule_changes:
+  print("We're going to the Super Bowl!")
+else:
+  print("I can't predict the future, but no, the Jaguars will never win the Super Bowl.")
