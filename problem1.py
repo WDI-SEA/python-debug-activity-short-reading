26e736be50d1982365900efd6c9cca90787f6296
kys3923 <kys3923@gmail.com>
2021-03-01 20:59:44 -0500

    solved all questions

---
+++ problem1.py
@@ -0,0 +1,37 @@
+offense = False
+defense = False
+rule_changes = False
+
+print("How are the Jags doing?\n")
+
+def get_offense():
+  offense = True
+  print("We have offense:", offense)
+
+def get_defense():
+  offense = True
+  defense = True
+  print("We have defense:", defense)
+
+  def get_rule_changes():
+    rule_changes = True
+    print("We have some rule changes:", rule_changes)
+
+    if offense and defense and rule_changes:
+      print("We're going to the Super Bowl!")
+    else:
+      print("I can't predict the future, but no, the Jaguars will never win the Super Bowl.")
+
+  if offense and defense:
+    get_rule_changes()
+  
+  
+
+get_offense()
+get_defense()
+
+
+
+
+
+
