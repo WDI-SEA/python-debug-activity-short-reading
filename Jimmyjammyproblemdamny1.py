7f0a6d225d236223416774d80f5d7b85e5ca51dc
unknown <dancesfire@gmail.com>
2021-03-02 08:33:36 -0800

    homework done

--- Jimmyjammyproblemdamny1.py
+++ Jimmyjammyproblemdamny1.py
@@ -4,34 +4,36 @@ rule_changes = False
 
 print("How are the Jags doing?\n")
 def get_offense():
+  global offense
   offense = True
   print("We have offense:", offense)
   return offense
   
 
 def get_defense():
+  global defense
   defense = True
   print("We have defense:", defense)
   return defense
 
 def get_rule_changes():
+    global rule_changes
     rule_changes = True
     print("We have some rule changes:", rule_changes)
     if offense and defense:
-                print('111', offense)
-                get_rule_changes()
+                print('Thanks for checking over my homework!', offense)
+               
     return rule_changes
 
 
 
-def team_win():
-    if offense and defense and rule_changes:
-        print("We're going to the Super Bowl!")
-    else:
-        print(offense)
-        print("I can't predict the future, but no, the Jaguars will never win the Super Bowl.")
-
 get_offense()
 get_defense()
 get_rule_changes()
-team_win()
+
+if offense and defense and rule_changes:
+        print("We're going to the Super Bowl!")
+else:
+        print(offense)
+        print("I can't predict the future, but no, the Jaguars will never win the Super Bowl.")
+
