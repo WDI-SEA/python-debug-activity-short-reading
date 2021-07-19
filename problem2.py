68bb6f29b21c626cfd1471c2ada14f08e405ecc0
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-07-19 18:58:58 -0500

    Starter code debugged.

--- problem2.py
+++ problem2.py
@@ -3,16 +3,20 @@ defense = False
 rule_changes = False
 
 def get_offense():
-  offense = True
+    global offense
+    offense = True
 
 def get_defense():
-  defense = True
+    global offense
+    global defense
+    defense = True
 
-  def get_rule_changes():
-    rule_changes = True
+    def get_rule_changes():
+        global rule_changes
+        rule_changes = True
 
-  if offense and defense:
-    get_rule_changes()
+    if offense and defense:
+        get_rule_changes()
 
 get_offense()
 get_defense()
@@ -23,6 +27,6 @@ print("We have defense:", defense)
 print("We have some rule changes:", rule_changes)
 
 if offense and defense and rule_changes:
-  print("We're going to the Super Bowl!")
+    print("We're going to the Super Bowl!")
 else:
-  print("I can't predict the future, but no, the Jaguars will never win the Super Bowl.")
+    print("I can't predict the future, but no, the Jaguars will never win the Super Bowl.")
